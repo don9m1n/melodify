a2efaf93a371cc1d607ce4e7bdec775df81549c8
Dongmin Kim <ddmkim94@gmail.com>
2024-03-08 16:56:57 +0900

    프로젝트 생성

---
+++ src/test/java/com/dmk/melodify/MelodifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmk.melodify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MelodifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
