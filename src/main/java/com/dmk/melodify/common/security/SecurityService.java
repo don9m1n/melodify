2d36705cc4602d9209d0867e0851976a45374916
Dongmin Kim <ddmkim94@gmail.com>
2024-03-09 14:11:32 +0900

    Feat: 로그인 구현
    - 시큐리티 폼 로그인 방식으록 구현

---
+++ src/main/java/com/dmk/melodify/common/security/SecurityService.java
@@ -0,0 +1,42 @@
+package com.dmk.melodify.common.security;
+
+import com.dmk.melodify.domain.member.entity.Member;
+import com.dmk.melodify.domain.member.repository.MemberRepository;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Slf4j
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class SecurityService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.debug("시큐리티 폼 로그인시 loadByUsername() 실행!: {}", username);
+        Member member = memberRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("not found member"));
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+
+        switch (member.getRole()) {
+            case MEMBER -> authorities.add(new SimpleGrantedAuthority("ROLE_MEMBER"));
+            case ADMIN -> authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+        }
+
+        return new User(member.getUsername(), member.getPassword(), authorities);
+    }
+
+}
--- src/main/java/com/dmk/melodify/domain/member/repository/MemberRepository.java
+++ src/main/java/com/dmk/melodify/domain/member/repository/MemberRepository.java
@@ -1,7 +1,9 @@
 package com.dmk.melodify.domain.member.repository;
 
 import com.dmk.melodify.domain.member.entity.Member;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByUsername(String username);
 }
