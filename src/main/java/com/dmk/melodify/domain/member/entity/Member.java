069acef232d74e7d34f8d30f7c8a5fda8eef14be
Dongmin Kim <ddmkim94@gmail.com>
2024-03-11 17:20:24 +0900

    Test: 회원가입 테스트
    - mockMvc를 사용해서 회원가입 테스트 코드 작성
    - 회원가입 후 로컬에 저장된 이미지 파일 삭제 처리

--- src/main/java/com/dmk/melodify/domain/member/entity/Member.java
+++ src/main/java/com/dmk/melodify/domain/member/entity/Member.java
@@ -1,6 +1,7 @@
 package com.dmk.melodify.domain.member.entity;
 
 import com.dmk.melodify.common.auditing.BaseEntity;
+import com.dmk.melodify.common.util.AppConfig;
 import com.dmk.melodify.domain.member.dto.JoinForm;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -8,12 +9,16 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import java.io.File;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 
 @Entity
+@Slf4j
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member extends BaseEntity {
@@ -30,6 +35,7 @@ public class Member extends BaseEntity {
 
     private String nickname;
 
+    @Setter
     private String profileImg;
 
     @Enumerated(EnumType.STRING)
@@ -45,14 +51,28 @@ public class Member extends BaseEntity {
         this.role = role;
     }
 
-    public static Member of(JoinForm form, String password) {
+    public static Member of(JoinForm form, String password, String profileImgRelPath) {
         return Member.builder()
                 .username(form.getUsername())
                 .password(password)
                 .email(form.getEmail())
                 .nickname(form.getNickname())
                 .role(MemberRole.MEMBER)
+                .profileImg(profileImgRelPath)
                 .build();
     }
 
+    public void removeProfileImgOnStorage() {
+        if (profileImg == null || profileImg.isEmpty()) {
+            return;
+        }
+
+        String profileImgPath = getProfileImgPath();
+        log.debug("profileImgPath: {}", profileImgPath);
+        new File(profileImgPath).delete();
+    }
+
+    private String getProfileImgPath() {
+        return AppConfig.FILE_DIR_PATH + "/" + profileImg;
+    }
 }
