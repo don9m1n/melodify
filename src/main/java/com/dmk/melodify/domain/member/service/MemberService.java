ffd9de2bf7d51953ad79fe9fd65eaa7576481cf7
Dongmin Kim <ddmkim94@gmail.com>
2024-03-11 17:19:34 +0900

    Feat: 프로필 업로드 기능 구현
    - 회원가입시 프로필 이미지를 업로드 하는 기능 구현
    - 설정 변수를 한 곳에서 관리하는 AppConfig 클래스 구현

--- src/main/java/com/dmk/melodify/domain/member/service/MemberService.java
+++ src/main/java/com/dmk/melodify/domain/member/service/MemberService.java
@@ -1,14 +1,23 @@
 package com.dmk.melodify.domain.member.service;
 
+import com.dmk.melodify.common.util.AppConfig;
 import com.dmk.melodify.domain.member.dto.JoinForm;
 import com.dmk.melodify.domain.member.entity.Member;
 import com.dmk.melodify.domain.member.repository.MemberRepository;
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
+@Slf4j
 @Transactional
 @RequiredArgsConstructor
 public class MemberService {
@@ -17,7 +26,42 @@ public class MemberService {
     private final PasswordEncoder passwordEncoder;
 
     public void join(JoinForm joinForm) {
-        Member member = Member.of(joinForm, passwordEncoder.encode(joinForm.getPassword()));
+
+        memberRepository.findByUsername(joinForm.getUsername()).ifPresent(it -> {
+            throw new RuntimeException("이미 가입된 회원입니다.");
+        });
+
+        MultipartFile profileImg = joinForm.getProfileImg();
+        log.debug("프로필 이미지가 없나요? {}", profileImg.isEmpty());
+
+        String profileImgDirName = "member";
+        String fileName = UUID.randomUUID() + ".png";
+        String profileImgDirPath = AppConfig.FILE_DIR_PATH + "/" + profileImgDirName;
+        String profileImgFilePath = profileImgDirPath + "/" + fileName;
+
+        new File(profileImgDirPath).mkdirs(); // 해당 폴더가 없는 경우 만들어준다.
+
+        try {
+            profileImg.transferTo(new File(profileImgFilePath));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        String profileImgRelPath = profileImgDirName + "/" + fileName;
+        Member member = Member.of(joinForm, passwordEncoder.encode(joinForm.getPassword()), profileImgRelPath);
         memberRepository.save(member);
     }
+
+    public void removeProfileImg(Member member) {
+        log.debug("profileImg: {}", member.getProfileImg());
+        member.removeProfileImgOnStorage();
+        member.setProfileImg(null);
+
+        memberRepository.save(member);
+    }
+
+    public Member getMemberById(Long memberId) {
+        return memberRepository.findById(memberId).orElseThrow(() -> new UsernameNotFoundException("존재하지 않는 회원입니다."));
+    }
+
 }
